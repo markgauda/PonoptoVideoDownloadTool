4674fe294b99dd0e48144c424427f30a2a913c9c
markgaud <markgaud@buffalo.edu>
2021-09-20 20:01:07 -0400

    Changed the input so you only need a URL

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 """
-This program takes in the path to an index.m3u8 file and a url
-This program then modifies the index.m3u8 file such that it
-    will prepend every line that dosent start with a '#' with the
-    url
+This program takes in the a url to an index.m3u8 file
+    and it will return a modified version of that file
+    hat vlc media player can use
 """
 
 
 import sys
+import urllib.request
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        pathToFileToChange = sys.argv[1]
-        url = sys.argv[2]
-        outputFileLocation = "./Output.m3u8"
-        with open(pathToFileToChange, "r") as inputFile:
+    if len(sys.argv) == 2:
+        url = sys.argv[1]
+        fileToModify = "tempFile.txt"
+        urllib.request(url, fileToModify)
+        urlSliceEnd = url.find("index.m3u8")
+        url = url[0:(urlSliceEnd -1)]
+
+        with(fileToModify, r) as inputFile:
             buffer = ""
             for line in inputFile:
                 if line[0] == '#':
                     buffer = buffer + line
                 else:
                     buffer = buffer + url + '/' + line
-
-        with open(outputFileLocation, "w") as outputFile:
-            outputFile.write(buffer)
-            
+             
         
     else:
-        print("invalid amount of arguments (Should be 2) (0 through 2)")
+        print("invalid amount of arguments (Should be 1) (0 through 1)")
         argumentNumber = 0
         for argument in sys.argv:
             print(f"{argumentNumber}, {argument}")
@@ -35,3 +35,5 @@ if __name__ == "__main__":
 
 
 
+
+    
